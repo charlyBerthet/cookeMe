41f9ed9279f3973aa64d2a391bbe1fae1b6246f4
charly.berthet <charlyberthet@orange.fr>
2016-05-23 15:03:58 +0200

    qlq classes

--- src/dao/instance/RecipesDao.java
+++ src/dao/instance/RecipesDao.java
@@ -1,5 +1,18 @@
 package dao.instance;
 
-public class RecipesDao {
+import java.util.ArrayList;
 
+import model.RecipeModelBean;
+
+public class RecipesDao {
+	public RecipesDao(String DB_HOST, String DB_PORT, String DB_NAME,String DB_USER,String DB_PWD) {
+		//TODO
+	}
+	public void addRecipe(RecipeModelBean recipe) {
+		//TODO
+	}
+	public ArrayList<RecipeModelBean> getAllRecipes() {
+		//TODO
+		return null;
+	}
 }

43f7e6ac7ad7dc6db6d576a9674a88580b363aeb
francoisDode <francois_dode@orange.fr>
2016-05-23 15:23:52 +0200

    suce ma bite

--- src/model/LoginBean.java
+++ src/model/LoginBean.java
@@ -1,5 +1,21 @@
 package model;
 
-public class LoginBean {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class LoginBean implements Serializable{
+	private String login;
+	private String pwd;
+	public LoginBean() { 
+		
+	}
+	
+	public String getLogin() {return login;}
+	public void setLogin(String login) {this.login = login;}
+	public String getPwd() {return pwd;}
+	public void setPwd(String pwd) {this.pwd = pwd;}
 }
--- src/model/UserSubmissionModelBean.java
+++ src/model/UserSubmissionModelBean.java
@@ -1,5 +1,12 @@
 package model;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
-public class UserSubmissionModelBean {
-
+@ManagedBean
+@RequestScoped //Durée de vue uniquement lors d'une requète
+//même propriétés que UserModelBean mais portée différente
+public class UserSubmissionModelBean extends UserModelBean{
+	
+	public UserSubmissionModelBean() {
+	}
 }

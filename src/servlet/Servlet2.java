e7742e66bc4bc013f463e22497d3bff240b4ca15
francoisDode <francois_dode@orange.fr>
2016-05-23 14:08:43 +0200

    servlet 2

--- src/servlet/Servlet2.java
+++ src/servlet/Servlet2.java
@@ -1,11 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.DB;
+import model.UserModel;
 
 /**
  * Servlet implementation class Servlet2
@@ -13,12 +17,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Servlet2")
 public class Servlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private DB db;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Servlet2() {
         super();
+        db = new DB();
         // TODO Auto-generated constructor stub
     }
 
@@ -26,7 +31,8 @@ public class Servlet2 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		UserModel user=new UserModel(request.getParameter("lastname"),request.getParameter("surname"),Integer.valueOf(request.getParameter("age")),request.getParameter("login"), request.getParameter("pwd"));
+		db.addUser(user);
 	}
 
 	/**

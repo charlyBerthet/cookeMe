6a76af3d050f4ac5830f7970dba06cfd553bb1cf
charly.berthet <charlyberthet@orange.fr>
2016-05-23 16:28:24 +0200

    RecipeDao Add

--- src/dao/instance/RecipesDao.java
+++ src/dao/instance/RecipesDao.java
@@ -1,18 +1,63 @@
 package dao.instance;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.RecipeModel;
 
 public class RecipesDao {
+	private Connection connection;
+	private String dB_HOST;
+	private String dB_PORT;
+	private String dB_NAME;
+	private String dB_USER;
+	private String dB_PWD;
+
 	public RecipesDao(String DB_HOST, String DB_PORT, String DB_NAME,String DB_USER,String DB_PWD) {
-		//TODO
+		dB_HOST= DB_HOST;
+		dB_PORT= DB_PORT;
+		dB_NAME= DB_NAME;
+		dB_USER= DB_USER;
+		dB_PWD= DB_PWD;
 	}
+
 	public void addRecipe(RecipeModel recipe) {
-		//TODO
+		PreparedStatement query;
+		try{
+			connection = connect();
+			String sql = "INSERT INTO cookbcf.recipe"
+						+"(description, title, expertise, nbpeople, duration, type)"
+						+"VALUES(?, ?, ?, ?, ?, ?)";
+			query = connection.prepareStatement(sql);
+			query.setString(1, recipe.getDescription());
+			query.setString(2, recipe.getTitle());
+			query.setInt(3, recipe.getExpertise());
+			query.setInt(4, recipe.getNbpeople());
+			query.setInt(5, recipe.getDuration());
+			query.setString(6, recipe.getType());
+			query.executeUpdate();
+			query.close();
+			connection.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
+	
 	public ArrayList<RecipeModel> getAllRecipes() {
 		//TODO
+		return null;
+	}
+	
+	
+	private Connection connect(){
+		try {
+			return java.sql.DriverManager.getConnection("jdbc:mysql://"+dB_HOST+":"+dB_PORT+"/"+dB_NAME,dB_USER, dB_PWD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
